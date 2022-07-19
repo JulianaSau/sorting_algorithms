7e90885fbb9e154badce7d276f83e8b358302e1e
Sau Juliana <julienyange@gmail.com>
2022-07-19 23:01:13 +0300

    added advanced tasks

--- sort.h
+++ sort.h
@@ -7,6 +7,7 @@
  */
 #include <stdbool.h>
 #include "stdio.h"
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -31,5 +32,15 @@ void swap(listint_t **head, listint_t *l, listint_t *r);
 void quick_sort(int *array, size_t size);
 void sort(int *arr, int left, int right, size_t size);
 int split(int *arr, int left, int right, size_t size);
+void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
 
 #endif /* SORT_H */
